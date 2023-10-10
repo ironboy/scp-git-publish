1b0a30ca1124b311024f5f887044393c544369e1
Ironboy <ironboy@users.noreply.github.com>
2023-10-10 13:12:55 +0200

    Update index.php - I am the bestest!

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello this is the dev branch/site that Mr Systemsson can change! Mr Systemsson is the bestest developer in the world!";
+echo "Hello this is the dev branch/site that Mr Systemsson can change! Developed 2023 by the bestest developer in the world, Mr Systemsson. I have all rights!";
